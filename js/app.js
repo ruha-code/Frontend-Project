d1b002d45d4e0e20ff7941cff1157fcd54e404a2
ruha-code <ruslanusen145@gmail.com>
2025-11-24 22:50:42 +0500

    add navbar with animations

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-console.log("Hello");
+const mobileMenu = document.querySelector('.mobile-menu');
+const toggle = document.querySelector('.menu-toggle');
+
+toggle.onclick = () => {
+    mobileMenu.classList.toggle('open');
+};
+
+mobileMenu.addEventListener('mouseleave', () => {
+    mobileMenu.classList.remove('open');
+});
+
+window.onresize = () => {
+    if (window.innerWidth > 768) {
+        mobileMenu.classList.remove('open');
+    }
+};
+
+
