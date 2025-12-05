22b52c4554a890966eced1446fa150f73e55d23b
ruha-code <ruslanusen145@gmail.com>
2025-12-05 15:29:19 +0500

    fix

--- js/app.js
+++ js/app.js
@@ -80,7 +80,7 @@ function updateNavState() {
             e.preventDefault();
             localStorage.removeItem('isLoggedIn'); 
             localStorage.removeItem('userProfile');
-            window.location.reload(); 
+            window.location.href = 'index.html';
         });
     }
 
